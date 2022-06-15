10ce0c40b29621af5c8cda6da4e96f936fbd057c
2Kelvin <kvin.mwangi254@gmail.com>
2022-06-15 19:20:14 +0300

    added all pages/components to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-// consistently 'routed' components [Header, Navbar & Footer]
+import Header from "./Components/Header";
+import Navbar from "./Components/Header";
+import Home from "./Components/Header";
+import NewPost from "./Components/Header";
+import PostsPage from "./Components/Header";
+import About from "./Components/Header";
+import Missing from "./Components/Header";
+import Footer from "./Components/Header";
 import { Route, Switch, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
-     Hey 
+      <Header />
+      <Navbar />
+      <Home />
+      <NewPost />
+      <PostsPage />
+      <About />
+      <Missing />
+      <Footer/>
     </div>
   );
 }
