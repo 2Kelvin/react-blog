a87a8917043a28f6421724182d8cbe4dbf7ff870
2Kelvin <kvin.mwangi254@gmail.com>
2022-06-15 19:23:42 +0300

    corrected import errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from "./Components/Header";
-import Navbar from "./Components/Header";
-import Home from "./Components/Header";
-import NewPost from "./Components/Header";
-import PostsPage from "./Components/Header";
-import About from "./Components/Header";
-import Missing from "./Components/Header";
-import Footer from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import NewPost from "./Components/NewPost";
+import PostsPage from "./Components/PostsPage";
+import About from "./Components/About";
+import Missing from "./Components/Missing";
+import Footer from "./Components/Footer";
 import { Route, Switch, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 
